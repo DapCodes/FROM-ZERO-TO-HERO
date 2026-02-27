50a5b318b679a016838dfcdf59c4b45850fe73ec
DapCodes <daffaramadhan929@gmail.com>
2026-02-28 00:39:38 +0700

    Add initial C++ source files for basic output and calculations
    - Created subs1.cpp to print a greeting message.
    - Created subs2.cpp to print multiple greeting messages in one line.
    - Created subs3.cpp to display the number of ducks owned by Pak Dengklek.
    - Created subs4.cpp to calculate and display the total number of ducks using addition.
    - Created subs5.cpp to demonstrate various output formats and calculations.
    - Created subs6.cpp to calculate and display the area and perimeter of a pen.

---
+++ Daffa Ramadhan/tlx/bab-1/subs4.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    int angka = 738 + 519;
+    std::cout << "Pak Dengklek memiliki " << angka << " ekor bebek." << std::endl;
+}
---
+++ Daffa Ramadhan/tlx/bab-1/subs6.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    // cetak luas kandang
+    int luasKandang = 364 * 79;
+    std::cout << luasKandang << std::endl;
+
+    // cetak keliling kandang
+    int kelilingKandang = 2 * (364 + 79);
+    std::cout << kelilingKandang << std::endl;
+}
